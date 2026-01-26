08f0cff248f75007e10550c3abe71d72dfe08dd5
ramom06 <rmartinespana@gmail.com>
2026-01-26 18:35:06 +0100

    Update CalculatorServiceTest.java

--- CalculadoraWeb/src/test/java/com/example/calculadoraweb/CalculatorServiceTest.java
+++ CalculadoraWeb/src/test/java/com/example/calculadoraweb/CalculatorServiceTest.java
@@ -17,7 +17,7 @@ public class CalculatorServiceTest {
     
     @Test
     void testAdd() {
-        assertEquals(9, service.add(4, 5));
+        assertEquals(12, service.add(4, 5));
     }
     
     @Test
